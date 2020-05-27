8e7cbb148b91d4972bb730de9822bad7089d121a
hassan <almasrur97@gmail.com>
2020-05-27 11:42:11 -0400

    added the frontend to the application

---
+++ build/precache-manifest.a6f156b7e6d1c857b11e9de3d86437ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c13d174ace466a64cf7809ff42acf56c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2f2bc7e78c48b1afc98",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "da9c83f8379307f89c63",
+    "url": "/static/js/2.6de0d325.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6de0d325.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2f2bc7e78c48b1afc98",
+    "url": "/static/js/main.9ee3dd17.chunk.js"
+  },
+  {
+    "revision": "37e42bc47805ba865915",
+    "url": "/static/js/runtime-main.fac6ad05.js"
+  }
+]);
