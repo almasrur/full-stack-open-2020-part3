e42f0e50bb2a15711b244e753d8d685315ab1463
hassan <almasrur97@gmail.com>
2020-05-28 16:06:46 -0400

    3.12: Command-line database

---
+++ mongo.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://almasrur:${password}@cluster0-hwbg9.mongodb.net/phonebook?retryWrites=true`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+mongoose.connection.on('connected', () => {
+    console.log('mongoose is connected')
+})
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+  })
+  
+const Person = mongoose.model('Person', personSchema)
+  
+if (process.argv.length === 3) {
+    Person.find({}).then(result => {
+        console.log('phonebook:')
+        result.forEach(p => {
+            console.log(p.name, p.number)
+        });
+        mongoose.connection.close()
+    })
+} else if (process.argv.length === 5) {
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4],
+      })
+      
+      person.save().then(result => {
+        console.log(`added ${result.name} number ${result.number} to phonebook`)
+        mongoose.connection.close()
+      })
+} else {
+    console.log('Please provide the person to be added as an argument: node mongo.js <password> <name> <number>')
+    mongoose.connection.close()
+}
+
